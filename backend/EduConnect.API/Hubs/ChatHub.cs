dfb16db2ca56c2437b3a73c10cfca0b80c06baee
SantiagoRS26 <105330429+SantiagoRS26@users.noreply.github.com>
2023-06-20 00:25:34 -0500

    Update ChatHub.cs

--- backend/EduConnect.API/Hubs/ChatHub.cs
+++ backend/EduConnect.API/Hubs/ChatHub.cs
@@ -1,9 +1,11 @@
-﻿using EduConnect.BLL.Interfaces;
+using EduConnect.BLL.Interfaces;
 using EduConnect.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace EduConnect.API.Hubs
 {
@@ -67,7 +69,14 @@ namespace EduConnect.API.Hubs
 
                 await _chatService.SaveMessage(message);
 
-                await Clients.User(receiver.Email).SendAsync("ReceiveMessage", message);
+                var messageData = new Dictionary<string, object>
+                {
+                    { "SenderId", message.SenderId },
+                    { "Message", message.Message },
+                    { "SentDate", message.SentDate }
+                };
+
+                await Clients.User(receiver.Email).SendAsync("ReceiveMessage", messageData);
             }
             catch (Exception ex)
             {
